290c06e3d0b44ba35ade741e399b909af92cbd92
Elif Kesen <elifkesen@gmail.com>
2023-12-14 12:22:51 +0100

    elifV2

--- src/Elif.java
+++ src/Elif.java
@@ -2,5 +2,6 @@ public class Elif {
     public static void main(String[] args) {
 
         System.out.println("Elif'in class'i");
+        System.out.println("Sana sari laleler aldim cicek pazarindan");
     }
 }
